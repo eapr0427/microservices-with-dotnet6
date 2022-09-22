bfb5e50fe0878093f6c94d82aec6b738523aa0ba
Andrés Pineda <eapinedar@hotmail.com>
2022-09-22 18:54:18 -0500

    Add cart

---
+++ JoviRestaurant/JoviRestaurant.Web/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using JoviRestaurant.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoviRestaurant.Web.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public IActionResult CartIndex()
+        {
+            return View();
+        }
+    }
+}
